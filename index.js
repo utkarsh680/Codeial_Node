4e26f8d93c976bb640d70c7fab5d4d88824a81bb
Utkarsh <singhrishu680@gmail.com>
2024-06-30 10:07:30 +0530

    add controllers

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const port = 8000;
 
+
+// user express router
+
+app.use('/', require('./routes'));
 app.listen(port, (err) =>{
     if(err) {
         console.log(`error ${err}`)
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const router = express.Router();
+
+const homeController = require("../controllers/home_controller");
+
+router.get("/", homeController.home);
+
+router.use("/users", require("./users"));
+
+router.use("/users", require("./posts"));
+
+
+
+
+module.exports = router;
