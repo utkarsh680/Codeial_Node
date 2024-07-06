48eef3d08bb49339055121d320caa8263818612e
Utkarsh <singhrishu680@gmail.com>
2024-07-06 23:36:43 +0530

    connect session to mongo

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -6,11 +6,17 @@ module.exports.profile = function (req, res) {
 };
 
 module.exports.signUp = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
   return res.render("user_sign_up", {
     title: "user sign up",
   });
 };
 module.exports.signIn = function (req, res) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/users/profile");
+  }
   return res.render("user_sign_in", {
     title: "user sign in",
   });
--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategy");
 
+const MongoStore = require("connect-mongo");
 app.use(express.urlencoded());
 app.use(cookieParser());
 
@@ -29,6 +30,7 @@ app.set("layout extractScripts", true);
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
+//mongo store is use to use the session cookie in the db
 app.use(
   session({
     name: "codeial",
@@ -37,15 +39,24 @@ app.use(
     saveUninitialized: false,
     resave: false,
     cookie: {
-      maxAge: (1000 * 60 * 100),
+      maxAge: 1000 * 60 * 100,
     },
+    store: MongoStore.create(
+      {
+        mongoUrl: "mongodb://127.0.0.1/codeial-development",
+        autoRemove: "disabled",
+      },
+      function (err) {
+        console.log(err || "connect mongodb setup ok");
+      }
+    ),
   })
 );
 
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use(passport.setAuthenticatedUser)
+app.use(passport.setAuthenticatedUser);
 
 // user express router
 app.use("/", require("./routes"));
