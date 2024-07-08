e671a3b0b9f18eeca0672edffe8090866c5d4870
Utkarsh <singhrishu680@gmail.com>
2024-07-09 02:05:02 +0530

    show post details

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,12 @@
-module.exports.home = function (req, res) {
-  return res.render("home", {
-    title: "Home",
-  });
+const Post = require("../models/post");
+module.exports.home = async function (req, res) {
+  try {
+    const post = await Post.find({});
+    return res.render("home", {
+      title: "Codeial | Home",
+      posts: post,
+    });
+  } catch (error) {
+    console.log("error in sending post data", error);
+  }
 };
