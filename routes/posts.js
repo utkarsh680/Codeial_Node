0d4eb2f2a55b4464e0d87f2233625af5e1fd7e9d
Utkarsh <singhrishu680@gmail.com>
2024-07-09 23:33:46 +0530

    checkAuthentication while posting

--- routes/posts.js
+++ routes/posts.js
@@ -3,7 +3,8 @@ const express = require("express");
 const router = express.Router();
 
 const postController = require("../controllers/post_controller");
+const passport = require("passport");
 
-router.post("/create", postController.create);
+router.post("/create", passport.checkAuthentication, postController.create);
 
 module.exports = router;
