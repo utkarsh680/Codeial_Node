7d2d92b736e5d354407160af0b0be639f15af8b5
Utkarsh <singhrishu680@gmail.com>
2024-07-09 23:06:42 +0530

    populate user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,20 @@
 const Post = require("../models/post");
+// module.exports.home = async function (req, res) {
+//   try {
+//     const post = await Post.find({});
+//     return res.render("home", {
+//       title: "Codeial | Home",
+//       posts: post,
+//     });
+//   } catch (error) {
+//     console.log("error in sending post data", error);
+//   }
+// };
+
+//populate the user of each post
 module.exports.home = async function (req, res) {
   try {
-    const post = await Post.find({});
+    const post = await Post.find({}).populate("user").exec();
     return res.render("home", {
       title: "Codeial | Home",
       posts: post,
