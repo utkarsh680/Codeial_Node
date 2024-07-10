d00939ce028bf88c80214fe78a0c7fe78749f295
Utkarsh <singhrishu680@gmail.com>
2024-07-10 21:54:51 +0530

    delete post

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,15 +1,34 @@
 const Post = require("../models/post");
+const Comment = require("../models/comment");
 
-  module.exports.create = async function (req, res) {
-    try {
-      const post = await Post.create({
-        content: req.body.content,
-        user: req.user._id,
-      });
+module.exports.create = async function (req, res) {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      user: req.user._id,
+    });
+    return res.redirect("back");
+  } catch (err) {
+    console.log("error in creating a post", err);
+    return res.redirect("back");
+  }
+};
+
+module.exports.destroy = async function (req, res) {
+  try {
+    const postId = req.params.id;
+
+    const post = await Post.findById(postId).exec();
+    //  .id means converting object id into strings
+    if (post.user == req.user.id) {
+      await post.deleteOne();
+      await Comment.deleteMany({ post: postId }).exec();
       return res.redirect("back");
-    } catch (err) {
-      console.log("error in creating a post", err);
+    } else {
       return res.redirect("back");
     }
-  };
-
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+};
--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
 const router = express.Router();
-
-const postController = require("../controllers/post_controller");
 const passport = require("passport");
 
+const postController = require("../controllers/post_controller");
+
 router.post("/create", passport.checkAuthentication, postController.create);
+router.get(
+  "/destroy/:id",
+  passport.checkAuthentication,
+  postController.destroy
+);
 
 module.exports = router;
