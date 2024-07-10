ef2a59e67f2b19fb98cdfe13896b94cb886e9bd8
Utkarsh <singhrishu680@gmail.com>
2024-07-10 22:42:42 +0530

    delete comment

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -20,3 +20,25 @@ module.exports.create = async function (req, res) {
     console.error("Error in creating a comment", err);
   }
 };
+module.exports.destroy = async function (req, res) {
+  try {
+    const commentId = req.params.id;
+    const comment = await Comment.findById(commentId).exec();
+    console.log(comment);
+    //  .id means converting object id into strings
+    if (comment.user == req.user.id) {
+      let postId = comment.post;
+
+      await comment.deleteOne();
+      await Post.findByIdAndUpdate(postId, {
+        $pull: { comments: commentId },
+      });
+      return res.redirect("back");
+    } else {
+      return res.redirect("back");
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+};
--- routes/comments.js
+++ routes/comments.js
@@ -6,5 +6,9 @@ const passport = require("passport");
 const commentsController = require("../controllers/comments_controller");
 
 router.post("/create", passport.checkAuthentication, commentsController.create);
-
+router.get(
+    "/destroy/:id",
+    passport.checkAuthentication,
+    commentsController.destroy
+  );
 module.exports = router;
