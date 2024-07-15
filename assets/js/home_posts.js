836681f07645fe4745315eb16324a4e5a2b66e4c
Utkarsh <singhrishu680@gmail.com>
2024-07-16 00:28:40 +0530

    showing post data using azax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -28,5 +28,41 @@
       });
     });
   };
+
+  //method to create a post in DOM
+  let newPostDom = function (post) {
+    return $(`<li id="post-${post._id}">
+        <p>
+            <small>
+            <a class="delete-post-button" href="/posts/destroy/${post._id}">X</a>
+            </small>
+            ${post.content}
+            <br />
+            <small><i>${post.user.name}
+        </i></small>
+        </p>
+
+        <div class="post-comments">
+           
+            <form action="/comments/create" method="POST">
+            <input
+                type="text"
+                name="content"
+                required
+                placeholder="typehere to add comment...."
+            />
+            <input type="hidden" name="post" value="${post._id}" />
+            <input type="submit" value="add comment" />
+            </form>
+
+          
+            <div class="post-comments-list">
+            <ul id="post-comments-${post._id}">
+            </ul>
+            </div>
+        </div>
+        </li>
+`);
+  };
   createPost();
 }
